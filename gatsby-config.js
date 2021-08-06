66b351f97dcab3bf466177d989d28eee2902d85e
Oqtarion <oqtarion@gmail.com>
2021-08-06 18:31:39 +0300

    Progressive Web Application

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,18 +22,21 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `sammy-muchiri`,
+        short_name: `sm`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#07080d`,
+        theme_color: `#ff0054`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icons/mm.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`, `/projects/*`],
+      },
+    },
   ],
 }
